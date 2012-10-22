240ecc3d056ff5f1dc1f7712c28d20530fa4cf43
Maxime ALAY-EDDINE <maxime.alayeddine@gmail.com>
2012-10-22 18:15:14 +0200

    Ajout structure html

--- SigArFix/export.rb
+++ SigArFix/export.rb
@@ -1,3 +1,6 @@
 def sigar_export()
-  UI.messagebox("Export")
+  dlgExport = UI::WebDialog.new("Show Sketchup.com", true,
+     "ShowSketchUpDotCom", 739, 641, 150, 150, true);
+   dlgExport.set_file "html/export.html"
+   dlgExport.show
 end
