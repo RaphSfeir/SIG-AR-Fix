69f314c8b64bdf6c6b43aefbaca80abb58d5e2f5
RaphSfeir <raphael.sfeir@eleves.ec-nantes.fr>
2012-10-22 18:19:33 +0200

    Début de l'écriture du fichier manage.html et manage.scs

--- SigArFix/manage.rb
+++ SigArFix/manage.rb
@@ -1,3 +1,5 @@
 def sigar_manage()
-  UI.messagebox("Gestion")
+  dlg = UI::WebDialog.new("Show Sketchup.com", true, "ShowSketchUpDotCom", 739, 641, 150, 150, true);
+  dlg.set_url "http://www.sketchup.com"
+  dlg.show
 end
