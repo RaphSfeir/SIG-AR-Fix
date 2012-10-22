b4e1f498a523b27f69eab0bf88642317c36abe9f
RaphSfeir <raphael.sfeir@eleves.ec-nantes.fr>
2012-10-22 18:29:40 +0200

    Lancement de la page Web corrigé avec le bon path de manage.rb

--- SigArFix/manage.rb
+++ SigArFix/manage.rb
@@ -1,5 +1,5 @@
 def sigar_manage()
   dlg = UI::WebDialog.new("Show Sketchup.com", true, "ShowSketchUpDotCom", 739, 641, 150, 150, true);
-  dlg.set_file "./SigArFix/html/manage.html"
+  dlg.set_file File.dirname(__FILE__) + "/html/manage.html"
   dlg.show
 end
