4ed9c71d5afca2d8061e7b72c0e55af3a06d84d8
RaphSfeir <raphael.sfeir@eleves.ec-nantes.fr>
2012-10-22 18:21:40 +0200

    Lancement de la page Web sur manage.rb

--- SigArFix/manage.rb
+++ SigArFix/manage.rb
@@ -1,5 +1,5 @@
 def sigar_manage()
   dlg = UI::WebDialog.new("Show Sketchup.com", true, "ShowSketchUpDotCom", 739, 641, 150, 150, true);
-  dlg.set_url "http://www.sketchup.com"
+  dlg.set_file "./SigArFix/html/manage.html"
   dlg.show
 end
