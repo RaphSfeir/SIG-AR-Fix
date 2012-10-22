26c8eaa727d10c383b30120a1671c40a192679c7
Maxime ALAY-EDDINE <maxime.alayeddine@gmail.com>
2012-10-22 18:29:11 +0200

    Ouverture de la page web d'export lors du clic sur le bouton d'export

--- SigArFix/export.rb
+++ SigArFix/export.rb
@@ -1,6 +1,6 @@
 def sigar_export()
   dlgExport = UI::WebDialog.new("Show Sketchup.com", true,
      "ShowSketchUpDotCom", 739, 641, 150, 150, true);
-   dlgExport.set_file "html/export.html"
+   dlgExport.set_file File.dirname(__FILE__)+"/html/export.html"
    dlgExport.show
 end
