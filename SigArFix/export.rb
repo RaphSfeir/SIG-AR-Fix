742d8e7ea86ec621a72f15c389f307bdcedfea7d
Maxime ALAY-EDDINE <maxime.alayeddine@gmail.com>
2012-11-05 18:17:20 +0100

    Mise à jour export.rb, export.html et export.css

--- SigArFix/export.rb
+++ SigArFix/export.rb
@@ -1,6 +1,31 @@
+# Function to export the current model with Sig-Ar info
 def sigar_export()
-  dlgExport = UI::WebDialog.new("Show Sketchup.com", true,
-     "ShowSketchUpDotCom", 739, 641, 150, 150, true);
-   dlgExport.set_file File.dirname(__FILE__)+"/html/export.html"
-   dlgExport.show
+  # Initilization of the window
+  export_width = 820
+  export_height = 600
+  c = Sketchup.active_model.active_view.center
+  dlgExport = UI::WebDialog.new("SIG-AR - Export your model", false, "ShowSigArFix_Export", export_width, export_height, c[0]-export_width/2, c[1]-export_height/2, true);
+  dlgExport.set_file File.dirname(__FILE__)+"/html/export.html"
+
+  # Will center the screen
+  dlgExport.add_action_callback("move") { |d, a|
+    xy, wh = a.split(":")
+  
+    x, y = xy.split(",")
+    x = x.to_i
+    y = y.to_i
+  
+    w, h = wh.split(",")
+    w = w.to_i
+    h = h.to_i
+  
+    d.set_position((x - w)/2, (y - h)/2)
+  }
+
+  dlgExport.min_width = export_width
+  dlgExport.min_height = export_height
+  dlgExport.max_width = export_width
+  dlgExport.max_height = export_height
+  # Show the window
+  dlgExport.show
 end
