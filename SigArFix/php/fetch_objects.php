289ceca3192714cd9b3a6a2e66c0ee3bb28f2a43
Maxime ALAY-EDDINE <maxime.alayeddine@gmail.com>
2012-11-24 22:09:52 +0100

    Fetch_objects : modification avec interface.php

--- SigArFix/php/fetch_objects.php
+++ SigArFix/php/fetch_objects.php
@@ -1,20 +1,19 @@
 <?php
 
-$user='sigar';
-$pass='rubyECN#2013';
-$dsn='pgsql:host=54.246.97.87;dbname=sigar_test';
+require_once "interface.php";
 $id_auteur = 1;
 //$id_auteur = base64_decode($_GET['idauth']);
 try {
     $dbh = new PDO($dsn, $user, $pass);
     $qry = $dbh->prepare("select nom_objet, date_creation, id_auteur from objet3d where id_auteur = ".$id_auteur.";");
-	$qry->execute();
-	 
-	$objets3d = $qry->fetchAll();
-	echo json_encode($objets3d);
+        $qry->execute();
+         
+        $objets3d = $qry->fetchAll();
+        echo json_encode($objets3d);
     $dbh = null;
 } 
-	catch (PDOException $e) {
-	echo json_encode(array("error" => $e->getMessage()));
+        catch (PDOException $e) {
+        echo json_encode(array("error" => $e->getMessage()));
     die();
 }
+?>
