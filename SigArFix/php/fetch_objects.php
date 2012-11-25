430159f8a44d4fc3d1dd718515c3b3efbb960c9d
RaphSfeir <raphael.sfeir@eleves.ec-nantes.fr>
2012-11-26 00:49:43 +0100

    Correction fetch_objects avec nouveaux champs de la table objects.

--- SigArFix/php/fetch_objects.php
+++ SigArFix/php/fetch_objects.php
@@ -5,11 +5,12 @@ $id_auteur = 1;
 //$id_auteur = base64_decode($_GET['idauth']);
 try {
     $dbh = new PDO($dsn, $user, $pass);
-    $qry = $dbh->prepare("select nom_objet, date_creation, id_auteur from objet3d where id_auteur = ".$id_auteur.";");
-        $qry->execute();
-         
-        $objets3d = $qry->fetchAll();
-        echo json_encode($objets3d);
+
+    $qry = $dbh->prepare("select name_object, date_creation, id_author from object3d where id_author = ".$id_auteur.";");
+    $qry->execute();
+     
+    $objets3d = $qry->fetchAll();
+    echo json_encode($objets3d);
     $dbh = null;
 } 
         catch (PDOException $e) {
