f8de3f5bef6bee04ad886549a6356fca40e03018
Maxime ALAY-EDDINE <maxime.alayeddine@gmail.com>
2012-11-28 23:24:44 +0100

    Export.html : modif css. Fetch_scenes : ajout fichier Upload_model : ajout fichier

---
+++ SigArFix/php/fetch_scenes.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "interface.php";
+$id_auteur = 1;
+//$id_auteur = base64_decode($_GET['idauth']);
+try {
+    $dbh = new PDO($dsn, $user, $pass);
+
+	$qry = $dbh->prepare("select A.*, B.name_object, B.date_creation, B.id_author from scene A, object3d B where A.id_author = ".$id_auteur." AND A.id_object3d = B.id_object3d;");    
+	$qry->execute();
+     
+    $objets3d = $qry->fetchAll();
+    echo json_encode($objets3d);
+    $dbh = null;
+} 
+        catch (PDOException $e) {
+        echo json_encode(array("error" => $e->getMessage()));
+    die();
+}
