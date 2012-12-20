edad72693401fc0de6919176a7ba05fb8385cd25
RaphSfeir <raphael.sfeir@eleves.ec-nantes.fr>
2012-12-20 20:18:10 +0100

    Insertion nouvelle catégorie : affiche d'un message

--- SigArFix/php/categories.php
+++ SigArFix/php/categories.php
@@ -3,25 +3,26 @@
 require_once "interface.php";
 $method = $_SERVER['REQUEST_METHOD'];
 header("Access-Control-Allow-Origin: *");
-header('Content-type: application/json');
 try {
     $dbh = new PDO($dsn, $user, $pass);
     switch ($method) {
       case 'GET':
+        header('Content-type: application/json');
         $qry = $dbh->prepare("select id_category, name_category from category;");
         $qry->execute();
         $categories = $qry->fetchAll();
         echo json_encode($categories);
         break;
       case 'POST':
-        var_dump($_POST);
+        header('Content-type: text/plain');
         if(is_null($_POST["category_name"])) {
-          echo json_encode(array('state' => 0));
+            echo "Impossible de r&eacute;cup&eacute;rer le nom de la cat&eacute;gorie."
         }
         else {
+          echo "Tentative d'insertion...";
           $qry = $dbh->prepare("insert into category(name_category) values ('".$_POST["category_name"]."');");
           $qry->execute();
-          echo json_encode(array('state' => $qry->rowCount(), 'value' => $_POST["category_name"]));
+          echo "Nouvelle cat&eacute;gorie cr&eacute;e !"
         }
         break;
       default:
