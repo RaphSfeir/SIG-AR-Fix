58e2c4fb4078dadf618d5148f10f2ce79ae14c8f
RaphSfeir <raphael.sfeir@eleves.ec-nantes.fr>
2012-12-21 07:51:19 +0100

    Prise en compte modif des rotations

--- SigArFix/php/edit_object.php
+++ SigArFix/php/edit_object.php
@@ -8,6 +8,9 @@ $id_scene = $_POST['id_scene'];
 $longitude = $_POST['longitude'];
 $latitude = $_POST['latitude'];
 $altitude = $_POST['altitude'];
+$rotation_x = $_POST['rotation_x'];
+$rotation_y = $_POST['rotation_y'];
+$rotation_z = $_POST['rotation_z'];
 
 $name_scene = addslashes($_POST['filename']);
 $name_object = addslashes($_POST['name']);
@@ -18,7 +21,7 @@ $id_auteur = 1;
 try {
     $dbh = new PDO($dsn, $user, $pass);
 	
-	$query_up_scene = "UPDATE scene SET gps_altitude = '".$altitude."', gps_longitude = '".$longitude."', gps_latitude = '".$latitude."', name_scene = '".$name_scene."' WHERE id_scene = ".$id_scene."";
+	$query_up_scene = "UPDATE scene SET gps_altitude = '".$altitude."', gps_longitude = '".$longitude."', gps_latitude = '".$latitude."', name_scene = '".$name_scene."', rotation_x = '".$rotation_x."', rotation_y = '".$rotation_y."', rotation_z = '".$rotation_z."' WHERE id_scene = ".$id_scene."";
 	$query_up_obj = "UPDATE object3d SET name_object = '".$name_object."' WHERE id_object3d = '".$id_object."'";
 
 	
